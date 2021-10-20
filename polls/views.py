f7e352751546175eb55756e27a74b3c7a763aa59
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-10-21 00:25:44 +0900

    render static html file

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Welcome to pswd-G")
+    return render(request, 'polls/index.html')
