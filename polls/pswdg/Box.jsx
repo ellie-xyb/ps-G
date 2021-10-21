d0f522255c71a893df64a3cf5bb7f9c0412f751f
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-10-22 00:08:58 +0900

    add a box container

---
+++ polls/pswdg/Box.jsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+
+export default function BoxSx() {
+  return (
+    <Box
+      sx={{
+        margin: '100px auto',
+        width: 800,
+        height: 500,
+        backgroundColor: '#7D886D',
+        borderRadius: 3,
+      }}
+    />
+  );
+}
+
---
+++ polls/pswdg/index.jsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import BoxSx from './Box'
+
+ReactDOM.render(<BoxSx />, document.querySelector('#styledbox'));
