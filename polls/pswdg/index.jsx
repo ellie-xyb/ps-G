2e05d593b88224d33ec3e26839f82a7481bae5b9
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-10-22 00:41:43 +0900

    set up background

--- polls/pswdg/index.jsx
+++ polls/pswdg/index.jsx
@@ -1,5 +1,43 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import BoxSx from './Box'
+import Box from '@mui/material/Box';
+import { padding } from '@mui/system';
+// import ResultBox from './ResultBox'
 
-ReactDOM.render(<BoxSx />, document.querySelector('#styledbox'));
+export default function PsGBox() {
+  return (
+    <Box
+      sx={{
+        margin: '100px auto',
+        width: 700,
+        height: 510,
+        border: '1px dashed grey',
+        borderRadius: 3,
+      }}
+    >
+        <Box
+          sx={{
+            width: 600,
+            height: 100,
+            backgroundColor: '#E2BFBD',
+            borderRadius: 3,
+            border: '1px dashed grey',
+            margin: '25px auto',
+
+          }}
+        />
+        <Box
+          sx={{
+            width: 600,
+            height: 330,
+            backgroundColor: '#FCFCFC',
+            borderRadius: 3,
+            border: '1px dashed grey',
+            margin: '10px auto',
+          }}
+        />
+    </Box>
+  );
+}
+
+ReactDOM.render(<PsGBox />, document.querySelector('#psg-box'));
