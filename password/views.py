c57cc47feb8ffd68783e22c4051007d6726c0541
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-10-30 20:07:18 +0900

    update generate password func

--- password/views.py
+++ password/views.py
@@ -5,10 +5,13 @@ import string
 import random
 
 
-def generated_password(length):
-    characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
-
-    # length = int("5")
+def generated_password(length, uppercase_check, lowercase_check, numbers_check, symbols_check):
+    # characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
+    upper_list = string.ascii_uppercase if uppercase_check else ''
+    lower_list = string.ascii_lowercase if lowercase_check else ''
+    numbers_list = string.digits if numbers_check else ''
+    symbols_list = "!@#$%^&*()" if symbols_check else ''
+    characters = list(upper_list + lower_list + numbers_list + symbols_list)
     random.shuffle(characters)
     password = []
     for i in range(length):
@@ -21,12 +24,18 @@ def generated_password(length):
 def gather_options(request):
     if request.method == 'POST':
         dic = {}
-        # print(request.body)
         json_data = json.loads(request.body)
+        print(request.body)
         if json_data:
             length = json_data.get('length')
             if length:
-                password = generated_password(length)
+                uppercase_check = json_data.get('uppercase')
+                lowercase_check = json_data.get('lowercase')
+                numbers_check = json_data.get('numbers')
+                symbols_check = json_data.get('symbols')
+                print(uppercase_check)
+                password = generated_password(
+                    length, uppercase_check, lowercase_check, numbers_check, symbols_check)
                 dic['password'] = password
                 dic = json.dumps(dic)
                 return HttpResponse(dic)
